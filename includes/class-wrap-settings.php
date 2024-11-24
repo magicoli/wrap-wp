e7eb887c5de39b06308dbf452903f527596fec22
Olivier van Helden <magic@hal.magiiic.com>
2024-11-24 22:47:22 +0100

    added fields to settings, external site base path and position menu on top

--- includes/class-wrap-settings.php
+++ includes/class-wrap-settings.php
@@ -15,7 +15,9 @@ class WrapSettings {
     }
 
     public static function add_admin_menu() {
-        add_menu_page(__('WRAP', 'magiiic-wrap'), 'WRAP', 'manage_options', 'wrap', [self::class, 'options_page'], 'dashicons-admin-generic');
+        $options = get_option('wrap_settings');
+        $position = isset($options['wrap_menu_position']) && $options['wrap_menu_position'] ? 2 : 80;
+        add_menu_page(__('WRAP', 'magiiic-wrap'), 'WRAP', 'manage_options', 'wrap', [self::class, 'options_page'], 'dashicons-admin-generic', $position);
         add_submenu_page('wrap', __('WRAP Settings', 'magiiic-wrap'), __('Settings', 'magiiic-wrap'), 'manage_options', 'wrap', [self::class, 'options_page']);
     }
 
@@ -30,12 +32,29 @@ class WrapSettings {
         );
 
         add_settings_field(
+            'wrap_menu_position',
+            __('Admin Menu Position', 'magiiic-wrap'),
+            [self::class, 'menu_position_render'],
+            'wrap_settings',
+            'wrap_settings_section'
+        );
+
+        add_settings_field(
             'wrap_base_url',
-            __('Base URL for External Site', 'magiiic-wrap'),
+            __('External Site Base URL', 'magiiic-wrap'),
             [self::class, 'base_url_render'],
             'wrap_settings',
             'wrap_settings_section'
         );
+
+        add_settings_field(
+            'wrap_base_path',
+            __('External Site Base Path', 'magiiic-wrap'),
+            [self::class, 'base_path_render'],
+            'wrap_settings',
+            'wrap_settings_section'
+        );
+
     }
 
     public static function base_url_render() {
@@ -48,13 +67,33 @@ class WrapSettings {
         <input type='text' name='wrap_settings[wrap_base_url]' value='<?php echo esc_attr($options['wrap_base_url']); ?>' class='<?php echo $class; ?>'>
         <?php
         if (get_option('wrap_base_url_error')) {
-            echo '<p class="description" style="color: red;">' . __('The URL is valid but not reachable.', 'magiiic-wrap') . '</p>';
+            echo '<p class="description error">' . __('URL is not reachable.', 'magiiic-wrap') . '</p>';
             delete_option('wrap_base_url_error');
         }
     }
 
+    public static function base_path_render() {
+        $options = get_option('wrap_settings');
+        ?>
+        <input type='text' name='wrap_settings[wrap_base_path]' value='<?php echo esc_attr($options['wrap_base_path']); ?>'>
+        <?php
+        if (get_option('wrap_base_path_error')) {
+            echo '<p class="description error">' . __('The path is not accessible.', 'magiiic-wrap') . '</p>';
+            delete_option('wrap_base_path_error');
+        }
+    }
+
+    public static function menu_position_render() {
+        $options = get_option('wrap_settings');
+        ?>
+        <input type='checkbox' name='wrap_settings[wrap_menu_position]' value='1' <?php checked(1, isset($options['wrap_menu_position']) ? $options['wrap_menu_position'] : 1); ?>>
+        <label for='wrap_settings[wrap_menu_position]'>
+            <?php _e('Up Where We Belong...', 'magiiic-wrap'); ?></label>
+        <?php
+    }
+
     public static function settings_section_callback() {
-        echo __('Enter the base URL for the external site.', 'magiiic-wrap');
+        echo __('Enter the settings for the WRAP plugin.', 'magiiic-wrap');
     }
 
     public static function options_page() {
@@ -62,6 +101,7 @@ class WrapSettings {
         <form action='options.php' method='post'>
             <h2><?php _e('WRAP Settings', 'magiiic-wrap'); ?></h2>
             <?php
+            settings_errors('wrap_settings'); // Afficher les erreurs ici
             settings_fields('wrap_settings');
             do_settings_sections('wrap_settings');
             submit_button();
@@ -73,35 +113,60 @@ class WrapSettings {
     public static function sanitize($input) {
         $output = [];
         if (isset($input['wrap_base_url'])) {
-            $url = esc_url_raw($input['wrap_base_url']);
+            $url = rtrim(esc_url_raw($input['wrap_base_url']), '/');
             if (filter_var($url, FILTER_VALIDATE_URL)) {
                 $output['wrap_base_url'] = $url;
                 if (!self::url_exists($url)) {
                     add_settings_error(
-                        'wrap_base_url',
+                        'wrap_settings',
                         'wrap_base_url_error',
-                        # This message appears below the field, so it's not necessary to specify "WRAP:"
-                        __('The URL is properly formatted but not reachable.', 'magiiic-wrap'),
+                        __('WRAP: The URL seems properly formatted but is not reachable.', 'magiiic-wrap'),
                         'error'
                     );
                     update_option('wrap_base_url_error', true);
                 }
             } else {
                 add_settings_error(
-                    'wrap_base_url',
+                    'wrap_settings',
                     'wrap_base_url_error',
-                    # Keep "WRAP:" in the message, it appears abofe the page and it's annoying to have a message not knowing which plugin sent it
                     __('WRAP: Invalid URL. Please enter a valid URL.', 'magiiic-wrap'),
                     'error'
                 );
             }
         }
+        if (!empty($input['wrap_base_path'])) {
+            $path = rtrim(sanitize_text_field($input['wrap_base_path']), '/');
+            $output['wrap_base_path'] = $path;
+            if (!self::path_exists($path)) {
+                error_log('Path does not exist: ' . $path);
+                add_settings_error(
+                    'wrap_settings',
+                    'wrap_base_path_error',
+                    __('The path is not accessible.', 'magiiic-wrap'),
+                    'error'
+                );
+                update_option('wrap_base_path_error', true);
+            } else {
+                delete_option('wrap_base_path_error');
+            }
+        }
+
+        $output['wrap_menu_position'] = isset($input['wrap_menu_position']) ? 1 : 0;
+
         return $output;
     }
 
     private static function url_exists($url) {
-        $headers = @get_headers($url);
-        return $headers && strpos($headers[0], '200') !== false;
+        $response = wp_remote_head($url);
+        if (is_wp_error($response)) {
+            return false;
+        }
+        $http_code = wp_remote_retrieve_response_code($response);
+        return in_array($http_code, [200, 301, 302, 401, 403]);
+    }
+
+    private static function path_exists($path) {
+        return is_dir($path) && is_readable($path);
     }
 }
 
